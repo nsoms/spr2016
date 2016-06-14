3e734d4f08822b49fc3b1c44e11ddb77fba82271
Nick Soms <n.l.soms@gmail.com>
2016-06-14 13:33:48 +0300

    Initial template commit

---
+++ classes/init.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once ('classes/helpers.php');
+
+
+/****/
+
+ini_set(''); // выводить все ошибки
+date_default_timezone_set('Europe/Moscow');
--- classes/user.php
+++ classes/user.php
@@ -65,3 +65,5 @@ class User {
 }
 
 $user = new User();
+
+date();
